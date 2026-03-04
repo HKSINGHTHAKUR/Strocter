b43b29a73cb61e7b36bc203b53e21f749562ba7d
harsh <harshkusingh9456@gmail.com>
2026-03-04 12:00:00 +0530

    fix: express 5 wildcard options path

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(cors({
     credentials: true,
     exposedHeaders: ["Content-Disposition"],
 }));
-app.options('*', cors());
+app.options(/(.*)/, cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
